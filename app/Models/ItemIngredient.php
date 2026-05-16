ea722e05ea8a70ecb9f5a71ee933ddc88585467e
Vikas Gupta <vikas.phpdev@gmail.com>
2026-05-16 17:29:10 +0530

    feat: Add soft delete functionality to Ingredient and ItemIngredient models

--- app/Models/ItemIngredient.php
+++ app/Models/ItemIngredient.php
@@ -3,9 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ItemIngredient extends Model
 {
+    use SoftDeletes;
     protected $fillable = [
         'item_id',
         'variant_id',
